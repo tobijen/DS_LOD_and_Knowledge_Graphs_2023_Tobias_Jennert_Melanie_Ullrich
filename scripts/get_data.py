b196e33547d335e7feff76334b0e43cd86f9f542
tobijen <tobiasjennert@web.de>
2024-01-02 23:36:09 +0100

    - deleted prints

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -6,22 +6,19 @@ from text_procesing import get_text
 url = 'https://api.openalex.org/works?sample=20'
 response = requests.get(url)
 
-print(response.status_code == 200)
-
 if response.status_code == 200:
     # Request was successful
     data = response.json()  # If the response is in JSON format
     for key, value in data.items():
             
             if key == 'results':
-                 print(value)
                  for result in value:
 
                         abstract = result["abstract_inverted_index"]
 
                         if abstract != None:
-                            print(abstract)
                             print(get_text(abstract))
+                            print("--------------------")
 
 else:
     # Request failed
