6055373813f14bd4d4e33a15dfbfe5b79cd36ff9
tobijen <tobiasjennert@web.de>
2024-01-31 16:13:02 +0100

    - Note: 1,3

--- scripts/extract_graph_data.py
+++ scripts/extract_graph_data.py
@@ -6,6 +6,11 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pathlib import Path
 import random
 
+#graph deps
+import networkx as nx
+import seaborn as sns
+from pyvis.network import Network
+
 
 from helpers.df_helpers import documents2Dataframe
 from helpers.df_helpers import df2Graph
@@ -112,4 +117,82 @@ dfg = (
 
 print(dfg)
 
-dfg.to_csv(outputdirectory/"graph_processed.csv", sep="|", index=False)
+if regenerate:
+    dfg.to_csv(outputdirectory/"graph_processed.csv", sep="|", index=False)
+
+# Calculate the NetworkX Graph
+nodes = pd.concat([dfg['node_1'], dfg['node_2']], axis=0).unique()
+print(nodes.shape)
+
+#create Graph
+G = nx.Graph()
+
+## Add nodes to the graph
+for node in nodes:
+    G.add_node(
+        str(node)
+    )
+
+## Add edges to the graph
+for index, row in dfg.iterrows():
+    G.add_edge(
+        str(row["node_1"]),
+        str(row["node_2"]),
+        title=row["edge"],
+        weight=row['count']/4
+    )
+
+communities_generator = nx.community.girvan_newman(G)
+top_level_communities = next(communities_generator)
+next_level_communities = next(communities_generator)
+communities = sorted(map(sorted, next_level_communities))
+print("Number of Communities = ", len(communities))
+print(communities)
+
+
+palette = "hls"
+
+## Now add these colors to communities and make another dataframe
+def colors2Community(communities) -> pd.DataFrame:
+    ## Define a color palette
+    p = sns.color_palette(palette, len(communities)).as_hex()
+    random.shuffle(p)
+    rows = []
+    group = 0
+    for community in communities:
+        color = p.pop()
+        group += 1
+        for node in community:
+            rows += [{"node": node, "color": color, "group": group}]
+    df_colors = pd.DataFrame(rows)
+    return df_colors
+
+
+colors = colors2Community(communities)
+print(colors)
+
+for index, row in colors.iterrows():
+    G.nodes[row['node']]['group'] = row['group']
+    G.nodes[row['node']]['color'] = row['color']
+    G.nodes[row['node']]['size'] = G.degree[row['node']]
+
+graph_output_directory = "./docs/index.html"
+
+net = Network(
+    notebook=False,
+    # bgcolor="#1a1a1a",
+    cdn_resources="remote",
+    height="900px",
+    width="100%",
+    select_menu=True,
+    # font_color="#cccccc",
+    filter_menu=False,
+)
+
+net.from_nx(G)
+# net.repulsion(node_distance=150, spring_length=400)
+net.force_atlas_2based(central_gravity=0.015, gravity=-31)
+# net.barnes_hut(gravity=-18100, central_gravity=5.05, spring_length=380)
+net.show_buttons(filter_=["physics"])
+
+net.show(graph_output_directory, notebook=False)
